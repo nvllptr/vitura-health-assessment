55d40a17f6ce26444812b4e1ebc52cb574a35092
Nikita Phung <nikitaphung2000@gmail.com>
2025-07-19 09:58:28 +1000

    Updated PatientDetails Page
    Created PatientDetails page showing the name, date of birth and the patient's prescription history.

--- vitura-frontend/src/pages/PatientDetails.tsx
+++ vitura-frontend/src/pages/PatientDetails.tsx
@@ -1,4 +1,64 @@
-export const PatientDetails = () => {
+import { useLocation, useNavigate, useParams } from "react-router-dom"
+import { useGetPatientByIdQuery } from "../services/patientsApi"
+import { Button, Card, Container, Table } from "react-bootstrap"
+import type { GetPrescriptionDto } from "../types/Prescription"
+import { routes } from "../routes"
 
-  return (<>PatientDetails</>)
+export const PatientDetails = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const patient = useGetPatientByIdQuery(Number(id))
+
+  const handleClickCreate = () => {
+    const route = routes.CREATE_PRESCRIPTION.replace(":id", id as string)
+    navigate(route, { state: { backgroundLocation: location } })
+  }
+
+  return (<>
+    <Container>
+      <h1>{patient.data?.fullName}</h1>
+      <p>Date of Birth: {patient.data?.dateOfBirth}</p>
+
+      <Card>
+        <Card.Header className="d-flex">
+          <div className="flex-grow-1">
+            Prescriptions
+          </div>
+          <div>
+            <Button onClick={handleClickCreate} className="btn-sm">
+              <i className="bi bi-plus-lg"></i>
+            </Button>
+          </div>
+        </Card.Header>
+        <Card.Body>
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Dosage</th>
+                <th>Date Prescribed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                patient.data?.prescriptions.map((prescription: GetPrescriptionDto) => {
+                  return (
+                    <tr key={prescription.id}>
+                      <td>{prescription.id}</td>
+                      <td>{prescription.drugName}</td>
+                      <td>{prescription.dosage}</td>
+                      <td>{prescription.datePrescribed}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
+  </>)
+
 }
